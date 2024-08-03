5d96a2a9c8cdb8004b129b79c726e2188b8a4194
vladlvov <105988540+vladlvov@users.noreply.github.com>
2024-08-03 14:02:43 +0500

    Add files via upload

---
+++ module_1_6.дораб.py
@@ -0,0 +1,14 @@
+my_dict = {"Vlad": 1997, 'Masha': 1999, 'Sasha': 1986}
+print(my_dict)
+print(my_dict.get('Petya'))
+my_dict.update({'Pasha': 1998 ,'Natasha': 1977})
+print(my_dict)
+print(my_dict.pop("Pasha"))
+print(my_dict)
+my_set = {1997, 1977, 1986, 1986, 1977, 1997, 1999}
+print(my_set)
+print(my_set.add(1988))
+print(my_set.add(1957))
+print(my_set)
+print(my_set.discard(1957))
+print(my_set)
